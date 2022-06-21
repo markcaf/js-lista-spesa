db73f976556ee6639bb9864c23b2f59b24311332
markcaf <marcocafassog@gmail.com>
2022-06-21 19:02:07 +0200

    While loop to create elements in the DOM

--- js/script.js
+++ js/script.js
@@ -15,3 +15,17 @@ const shopList = [
     "olio extravergine di oliva"
 ];
 console.table(shopList);
+
+// Assegnazione della variabile cartElement al contenitore dei nostri list-item
+const cartElement = document.getElementById("cart_element");
+console.log(cartElement);
+
+// Ciclo while per creare in DOM gli elementi list-item, con aggiunta classi per lo stile
+let i = 0;
+while (i <= shopList.length - 1){
+    const shopElement = document.createElement("button");
+    shopElement.classList.add("list-group-item", "list-group-item-action")
+    shopElement.innerText = shopList[i];
+    cartElement.append(shopElement);
+    i++;
+};
