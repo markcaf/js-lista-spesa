4c96ddb49687ad2f71d8718bfd3831aad381997b
markcaf <marcocafassog@gmail.com>
2022-06-21 18:59:13 +0200

    Array's declaration of the shopping list

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// Dichiarazione della variabile array della lista della spesa
+const shopList = [
+    "latte parzialmente scremato",
+    "yogurt magro",
+    "yogurt greco",
+    "ricotta",
+    "parmigiano reggiano",
+    "arrosto di tacchino o di pollo",
+    "bresaola",
+    "prosciutto crudo",
+    "prosciutto cotto",
+    "pane",
+    "pasta",
+    "riso",
+    "olio extravergine di oliva"
+];
+console.table(shopList);
